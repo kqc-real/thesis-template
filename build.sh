cac4185a0f5b1a07dc3011ff2dd64244d3a2687b
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel.ext@iu.org>
2026-01-26 17:41:38 +0100

    fix: Remove obsolete glossary and style files; update build script to exclude version control and common directories

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ MAIN="Artikel"
 if [ "$1" == "clean" ]; then
   echo "🧹 Bereinige temporäre Dateien..."
   # Standard LaTeX temporäre Dateien + Glossaries/Biber/MakeIndex Dateien
-  find . -type f \( -name "*.aux" -o -name "*.bbl" -o -name "*.bcf" -o -name "*.blg" -o -name "*.toc" -o -name "*.lof" -o -name "*.lot" -o -name "*.idx" -o -name "*.ilg" -o -name "*.ind" -o -name "*.out" -o -name "*.log" -o -name "*.run.xml" -o -name "*.lol" -o -name "*.synctex.gz" -o -name "*.fls" -o -name "*.fdb_latexmk" -o -name "*.nlo" -o -name "*.nls" -o -name "*.glo" -o -name "*.gls" -o -name "*.glg" -o -name "*.glsdefs" -o -name "*.ist" -o -name "*.acn" -o -name "*.acr" -o -name "*.alg" -o -name "*.toc2" \) -delete
+  # Exclude version-control and large common directories to avoid accidental deletions
+  find . \( -path "./.git" -o -path "./.github" -o -path "./node_modules" -o -path "./build" \) -prune -o -type f \( -name "*.aux" -o -name "*.bbl" -o -name "*.bcf" -o -name "*.blg" -o -name "*.toc" -o -name "*.lof" -o -name "*.lot" -o -name "*.idx" -o -name "*.ilg" -o -name "*.ind" -o -name "*.out" -o -name "*.log" -o -name "*.run.xml" -o -name "*.lol" -o -name "*.synctex.gz" -o -name "*.fls" -o -name "*.fdb_latexmk" -o -name "*.nlo" -o -name "*.nls" -o -name "*.glo" -o -name "*.gls" -o -name "*.glg" -o -name "*.glsdefs" -o -name "*.ist" -o -name "*.acn" -o -name "*.acr" -o -name "*.alg" -o -name "*.toc2" \) -delete
   # remove common helper files/folders created by scripts
   rm -f build.txt build_output.txt
   rm -rf build
